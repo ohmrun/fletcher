0f1a81961cd7af12972baff5acb39fe11a8e7f7d
0b1kn00b <polysemantic@gmail.com>
2021-12-15 14:48:04 +0100

    Sync

---
+++ src/main/haxe/eu/ohmrun/fletcher/term/Sync.hx
@@ -0,0 +1,8 @@
+package eu.ohmrun.fletcher.term;
+
+abstract class Sync<P,Pi,E> implements  FletcherApi<P,Pi,E> {
+  public function defer(p:P,cont:Terminal<Pi,E>):Work{
+    return cont.receive(cont.issue(apply(p)));
+  }
+  abstract function apply(v:P):ArwOut<Pi,E>;
+}
